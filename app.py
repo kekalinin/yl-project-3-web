097493671d30c2fc053cdd9ee661ec6d9b78fdb0
Kirill K. <k.e.kalinin@gmail.com>
2024-04-25 21:16:05 +0300

    Добавил вход/выход на сайт для пользователя

--- app.py
+++ app.py
@@ -1,41 +1,89 @@
-from flask import Flask
-from flask import render_template, redirect
-from werkzeug.security import generate_password_hash
+from flask import (
+    Flask,
+    render_template,
+    redirect,
+    url_for,
+    flash
+)
+from flask_login import (
+    logout_user,
+    LoginManager,
+    login_user,
+    current_user,
+    # login_required
+)
 
 import db_session
-from forms import RegisterForm, LoginForm
-from models import User
+import forms
+import models
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_super_secret_key'
 
+# Иницализируем LoginManager, который управляет процессом авторизации пользователя.
+lm = LoginManager(app)
+# Указываем на какой странице будет логин для пользователя.
+lm.login_view = 'login'
+
+
+@lm.user_loader
+def load_user(id):
+    """
+    Функция для получения пользователя из БД
+    """
+    s = db_session.create_session()
+    return s.get(models.User, int(id))
+
 
 @app.route('/')
-def root():
+def index():
     return render_template("index.html")
 
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
-    form = RegisterForm()
+    """
+    Создание пользователя
+    """
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = forms.RegisterForm()
     if form.validate_on_submit():
-        user = User()
-        user.login = form.login.data
-        user.username = form.username.data
-        user.hashed_password = generate_password_hash(form.password1.data)
-        db_sess = db_session.create_session()
-        db_sess.add(user)
-        db_sess.commit()
+        u = models.User(username=form.username.data, login=form.login.data)
+        u.set_password(form.password.data)
+        s = db_session.create_session()
 
-        return redirect('/login')
-
-    return render_template('register.html', form=form)
+        s.add(u)
+        s.commit()
+        flash('Поздравляем! Вы теперь зарегистрированный пользователь!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Регистрация нового пользователя', form=form)
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    form = LoginForm()
+    """
+    Вход в приложение
+    """
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = forms.LoginForm()
     if form.validate_on_submit():
-        return redirect('/')
+        s = db_session.create_session()
+        usr = s.scalar(
+            s.query(models.User).where(models.User.login == form.login.data))
+        if usr is None or not usr.check_password(form.password.data):
+            flash('Неверный логин или пароль')
+            return redirect(url_for('login'))
+        login_user(usr, remember=form.remember_me.data)
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Вход в альбом', form=form)
 
-    return render_template('login.html', form=form)
+
+@app.route('/logout')
+def logout():
+    """
+    Выход пользователя
+    """
+    logout_user()
+    return redirect(url_for('index'))
--- forms.py
+++ forms.py
@@ -1,31 +1,35 @@
+import sqlalchemy
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, EqualTo, Length
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, EqualTo
 from wtforms.validators import ValidationError
+
+import db_session
+from models import User
 
 
 class RegisterForm(FlaskForm):
-    class Meta:
-        locales = ['ru_RU', 'ru']
-
+    """
+    Форма регистрации нового пользователя
+    """
     username = StringField('Имя', validators=[DataRequired()])
     login = StringField('Логин', validators=[DataRequired()])
-    password1 = PasswordField('Пароль', validators=[
-        DataRequired(),
-        Length(min=6, max=10)])
-    password2 = PasswordField('Пароль (подтверждение)', validators=[
-        DataRequired(),
-        Length(min=6, max=10),
-        EqualTo('password1', message='Пароли должны совпадать')])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    password2 = PasswordField('Пароль (подтверждение)', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Зарегистрироваться')
+
+    def validate_login(self, login):
+        s = db_session.create_session()
+        user = s.scalar(sqlalchemy.select(User).where(User.login == login.data))
+        if user is not None:
+            raise ValidationError('Такой логин уже занят. Придумайте другой Ж:)')
 
 
 class LoginForm(FlaskForm):
-    username = StringField('Логин', validators=[DataRequired()])
+    """
+    Форма логина
+    """
+    login = StringField('Логин', validators=[DataRequired()])
     password = PasswordField('Пароль', validators=[DataRequired()])
-    submit = SubmitField('Зарегистрироваться')
-
-    def validate_username(self, username):
-        db_sess = db_session.create_session()
-        if db_sess.query(User).filter(User.username == username.data).count() == 0:
-            raise ValidationError('Такого пользователя нет!')
+    remember_me = BooleanField('Запомнить меня')
+    submit = SubmitField('Войти')
